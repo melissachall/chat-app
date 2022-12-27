39a382699e079a25044d0ef3b44ffddf009d418a
yoitsmel <melissachallam2@gmail.com>
2022-12-27 21:10:31 +0100

    chat project

---
+++ public/src/pages/Register.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import styled from 'styled-components';
+
+
+function Register() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert("form");
+    }
+    const handleChange = (event) => {}
+    return(
+    <FormContainer>
+        <form onSubmit={(event)=>handleSubmit(event)}>
+        <div className="brand">
+                <img src="" alt=""/>
+                <h1>Snappy</h1>
+            </div>
+            <input type="text" 
+            placeholder="Username" 
+            name="username" 
+            onChange={(e) => handleChange(e)}
+            />
+            <input 
+            type="email" 
+            placeholder="Email" 
+            name="email" 
+            onChange={(e) => handleChange(e)}
+            />
+            <input 
+            type="password" 
+            placeholder="Password" 
+            name="password"
+            onChange={(e) => handleChange(e)}
+            />
+            <input 
+            type="password" 
+            placeholder="Confirm Password" 
+            name="confirmPassword"  
+            onChange={(e) => handleChange(e)}
+            />
+            <button type="submit">Create User</button>
+            <span>already have an account ? <Link to="/login">Login</Link></span>
+        </form>
+       
+     </FormContainer>   
+  );
+const FormContainer = styled.div;
+
+};
+
+export default Register();
+
+
+
---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+
+const app = express();
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("DB Connetion Successfull");
+    })
+    .catch((err) => {
+        console.log(err.message);
+    });
+
+
+const server = app.listen(process.env.PORT, () =>
+    console.log('server listening on port', process.env.PORT)
+);
